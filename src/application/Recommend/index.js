b114d3fb60652a3f53c7246319f89dc61d07eefc
wangjojo <weiainijiujiu@126.com>
2020-09-08 17:37:21 +0800

    推荐模块3: 从零开发数据层

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,9 +1,11 @@
 // src/appliction/Home/index.js
-import React from 'react'
+import React, { useEffect } from 'react'
 import Slider from '../../components/slider'
 import RecommendList from '../../components/list'
 import Scroll from '../../components/scroll'
 import styled from'styled-components';
+import { connect } from "react-redux";
+import * as actionTypes from './store/actionCreators';
 
 export const Content = styled.div`
   position: fixed;
@@ -11,8 +13,16 @@ export const Content = styled.div`
   bottom: 0;
   width: 100%;
 `
-function Recommend() {
-  const bannerList = [1, 2, 3, 4].map(item => {
+function Recommend(props) {
+  const { bannerList, recommendList} = props
+  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
+  useEffect(() => {
+    getBannerDataDispatch()
+    getRecommendListDataDispatch()
+  }, [])
+  const bannerListJS = bannerList ? bannerList.toJS() : []
+  const recommendListJS = recommendList ? recommendList.toJS() : []
+  /*   const bannerList = [1, 2, 3, 4].map(item => {
     return { imageUrl: 'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg' }
   })
   const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => {
@@ -22,17 +32,31 @@ function Recommend() {
       playCount: 17171122,
       name: '朴树、许巍、李健、郑钧、老狼、赵雷'
     }
-  })
+  }) */
   return (
     <Content>
       <Scroll className="list">
         <div>
-          <Slider bannerList={bannerList}></Slider>
-          <RecommendList recommendList={recommendList}></RecommendList>
+          <Slider bannerList={bannerListJS}></Slider>
+          <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
     </Content>
   )
 }
 
-export default React.memo(Recommend)
+const mapStateToProps = (state) => ({
+  // 不要在这里将数据 toJS
+  // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
+  bannerList: state.getIn(['recommend', 'bannerList']),
+  recommendList: state.getIn(['recommend', 'recommendList'])
+})
+const mapDispatchToProps = dispatch => ({
+  getBannerDataDispatch() {
+    dispatch(actionTypes.getBannerlist())
+  },
+  getRecommendListDataDispatch() {
+    dispatch(actionTypes.getRecommendList())
+  }
+})
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Recommend))
---
+++ src/application/Recommend/store/actionCreators.js
@@ -0,0 +1,28 @@
+import * as actionTypes from './constants'
+import { fromJS } from 'immutable'
+import { getBannerRequest, getRecommendListRequest } from '../../../api/request'
+
+
+export const changeBannerList = (data) => ({
+  type: actionTypes.CHANGE_BANNER,
+  data: fromJS(data)
+}) 
+export const changeRecommendList = (data) => ({
+  type: actionTypes.CHANGE_RECOMMEND_LIST,
+  data: fromJS(data)
+})
+
+export const getBannerlist = () => {
+  return (dispatch) => {
+    getBannerRequest().then(data => {
+      dispatch(changeBannerList(data))
+    })
+  }
+}
+export const getRecommendList = () => {
+  return (dispatch) => {
+    getRecommendListRequest().then(data => {
+      dispatch(changeRecommendList(data))
+    })
+  }
+}
